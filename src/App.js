345db495304874924dde8b9fdac80dc6572d68ac
markclements225 <mcleme9@gmail.com>
2022-01-21 09:56:32 -0600

    added more styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Bayou Big Screen!</h1>
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
